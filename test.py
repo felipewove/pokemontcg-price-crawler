a519385030152b91ea6abc36806853bf03dca8bc
Felipe Brigatto <fbrigatto@nic.br>
2018-11-27 16:24:00 -0200

    reads argument + fix when there is variation image

--- test.py
+++ test.py
@@ -1,20 +1,26 @@
+import sys
+
 import requests
 from bs4 import BeautifulSoup
-from pokemontcgsdk import Card, Set, Subtype, Supertype, Type
+# from pokemontcgsdk import Card, Set
 
-col = Set.find('sm8')
-print(vars(col))
+# collection = Set.find('sm8')
 
-collection = 'LTD'
-url = "https://ligapokemon.com.br/?view=cards/search&card=ed={}&page=1".format(collection)
+# LigaPokemon paginates by 30
+# total_pages = int(collection.total_cards / 30) + 1
+
+collection_arg = sys.argv[1]
+url = "https://ligapokemon.com.br/?view=cards/search&card=ed={}&page=1".format(collection_arg)
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-rows = soup.find('table', id='cotacao-busca').find_all('tr')
+# soup = BeautifulSoup(open("backup.html", "r"), 'html.parser')
 
+rows = soup.find('table', id='cotacao-busca').find_all('tr')
+# first row is table header, so ignore it
 for row in rows[1:]:
     columns = row.find_all('td')
     print(">>>>>>>>>>>>>>>>>>>")
     print("Nome: ", row.find(class_='preto').string)
-    print("Preco menor: ", row.find(class_='preMen').string)
-    print("Preco medio: ", row.find(class_='preMed').string)
-    print("Preco maior: ", row.find(class_='preMai').string)
+    print("Preco menor: ", row.find(class_='preMen').p.contents[0])
+    print("Preco medio: ", row.find(class_='preMed').p.contents[0])
+    print("Preco maior: ", row.find(class_='preMai').p.contents[0])
