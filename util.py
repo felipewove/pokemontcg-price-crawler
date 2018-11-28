3af7ec7f8f82dc162e844b65341eedbef25770a9
Felipe Brigatto <fbrigatto@nic.br>
2018-11-28 18:52:09 -0200

    separetes logic at util.py

---
+++ util.py
@@ -0,0 +1,14 @@
+def parse_names(names):
+    splitted_names = names.split(" / ")
+    name_pt = splitted_names[0]
+    name_en = splitted_names[1] if len(splitted_names) > 1 else ""
+    return name_pt, name_en
+
+
+def split_name_code(name_ligapokemon):
+    names = name_ligapokemon.split(" (")[0]
+    name_pt, name_en = parse_names(names)
+
+    code = name_ligapokemon.split(" (")[1][:-1]
+
+    return {"name_pt": name_pt, "name_en": name_en, "code": code}
