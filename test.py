c5aea014b675da207ef1adf61393cc818178535c
Felipe Brigatto <fbrigatto@nic.br>
2018-11-28 18:14:03 -0200

    salving to csv + parsing name to get names in portugues and english and get code from card

--- test.py
+++ test.py
@@ -1,8 +1,32 @@
+import csv
 import sys
 
 import requests
 from bs4 import BeautifulSoup
 # from pokemontcgsdk import Card, Set
+
+
+def parse_names(names):
+    splitted_names = names.split(' / ')
+    name_pt = splitted_names[0]
+    name_en = splitted_names[1] if len(splitted_names) > 1 else ''
+    return name_pt, name_en
+
+
+def split_name_code(name_ligapokemon):
+    names = name_ligapokemon.split(' (')[0]
+    name_pt, name_en = parse_names(names)
+
+    code = name_ligapokemon.split(' (')[1][:-1]
+
+    return {
+        "name_pt": name_pt,
+        "name_en": name_en,
+        "code": code
+    }
+
+
+writer = csv.writer(open('export.csv', 'w', newline=''))
 
 # collection = Set.find('sm8')
 
@@ -18,9 +42,13 @@ soup = BeautifulSoup(page.content, 'html.parser')
 rows = soup.find('table', id='cotacao-busca').find_all('tr')
 # first row is table header, so ignore it
 for row in rows[1:]:
-    columns = row.find_all('td')
-    print(">>>>>>>>>>>>>>>>>>>")
-    print("Nome: ", row.find(class_='preto').string)
-    print("Preco menor: ", row.find(class_='preMen').p.contents[0])
-    print("Preco medio: ", row.find(class_='preMed').p.contents[0])
-    print("Preco maior: ", row.find(class_='preMai').p.contents[0])
+    splitted_name = split_name_code(row.find(class_='preto').string)
+    content = {
+        'name_pt': splitted_name['name_pt'],
+        'name_en': splitted_name['name_en'],
+        'code': splitted_name['code'],
+        'price_min': row.find(class_='preMen').p.contents[0],
+        'price_avg': row.find(class_='preMed').p.contents[0],
+        'price_max': row.find(class_='preMai').p.contents[0]
+    }
+    writer.writerow(content.values())
