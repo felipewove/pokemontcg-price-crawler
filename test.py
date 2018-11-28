252716124bdf93695563a04f847c5f6412c35dd1
Felipe Brigatto <fbrigatto@nic.br>
2018-11-28 18:52:34 -0200

    paginates crawler

--- test.py
+++ test.py
@@ -3,25 +3,9 @@ import sys
 
 import requests
 from bs4 import BeautifulSoup
+from util import split_name_code
 
 # from pokemontcgsdk import Card, Set
-
-
-def parse_names(names):
-    splitted_names = names.split(" / ")
-    name_pt = splitted_names[0]
-    name_en = splitted_names[1] if len(splitted_names) > 1 else ""
-    return name_pt, name_en
-
-
-def split_name_code(name_ligapokemon):
-    names = name_ligapokemon.split(" (")[0]
-    name_pt, name_en = parse_names(names)
-
-    code = name_ligapokemon.split(" (")[1][:-1]
-
-    return {"name_pt": name_pt, "name_en": name_en, "code": code}
-
 
 writer = csv.writer(open("export.csv", "w", newline=""))
 
@@ -31,23 +15,25 @@ writer = csv.writer(open("export.csv", "w", newline=""))
 # total_pages = int(collection.total_cards / 30) + 1
 
 collection_arg = sys.argv[1]
-url = "https://ligapokemon.com.br/?view=cards/search&card=ed={}&page=1".format(
-    collection_arg
-)
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-# soup = BeautifulSoup(open("backup.html", "r"), 'html.parser')
-
-rows = soup.find("table", id="cotacao-busca").find_all("tr")
-# first row is table header, so ignore it
-for row in rows[1:]:
-    splitted_name = split_name_code(row.find(class_="preto").string)
-    content = {
-        "name_pt": splitted_name["name_pt"],
-        "name_en": splitted_name["name_en"],
-        "code": splitted_name["code"],
-        "price_min": row.find(class_="preMen").p.contents[0],
-        "price_avg": row.find(class_="preMed").p.contents[0],
-        "price_max": row.find(class_="preMai").p.contents[0],
-    }
-    writer.writerow(content.values())
+current_page = 1
+total_pages = None
+url = "https://ligapokemon.com.br/?view=cards/search&card=ed={}&page={}"
+while current_page:
+    page = requests.get(url.format(collection_arg, current_page))
+    soup = BeautifulSoup(page.content, "html.parser")
+    # soup = BeautifulSoup(open("backup.html", "r"), 'html.parser')
+    total_pages = int(soup.find("li", id="paginacao-1").b.contents[0]) / 30 + 1
+    rows = soup.find("table", id="cotacao-busca").find_all("tr")
+    # first row is table header, so ignore it
+    for row in rows[1:]:
+        splitted_name = split_name_code(row.find(class_="preto").string)
+        content = {
+            "name_pt": splitted_name["name_pt"],
+            "name_en": splitted_name["name_en"],
+            "code": splitted_name["code"],
+            "price_min": row.find(class_="preMen").p.contents[0],
+            "price_avg": row.find(class_="preMed").p.contents[0],
+            "price_max": row.find(class_="preMai").p.contents[0],
+        }
+        writer.writerow(content.values())
+    current_page = current_page + 1 if current_page <= total_pages else None
