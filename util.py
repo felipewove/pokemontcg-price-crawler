bbc39d82ae4c71aaea2c7469e8ef99e4309ed914
brigattovictor <53950412+brigattovictor@users.noreply.github.com>
2019-11-24 10:17:34 -0300

    Update util.py
    Co-Authored-By: Felipe Brigatto <felipebrigatto@hotmail.com>

--- util.py
+++ util.py
@@ -9,7 +9,7 @@ def split_name_code(name_ligapokemon):
     names = ' '.join(name_ligapokemon.split(" ")[:-1])
     name_pt, name_en = parse_names(names)
 
-    code = name_ligapokemon.split(" ")[-1].split("/")[0][1:]
+    code = name_ligapokemon.split(" (")[-1].split("/")[0]
 
     return {"name_pt": name_pt, "name_en": name_en, "code": code}
 
